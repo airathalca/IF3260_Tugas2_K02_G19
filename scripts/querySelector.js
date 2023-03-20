b86b44d93c8058eedf91bbe3dde42cbf09bd7f4f
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-03-21 02:32:26 +0700

    add tools for transformation

---
+++ scripts/querySelector.js
@@ -0,0 +1,27 @@
+export var value = {
+  value_transX: document.querySelector("#value-x"),
+  value_transY: document.querySelector("#value-y"),
+  value_transZ: document.querySelector("#value-z"),
+  value_angleX: document.querySelector("#value-angleX"),
+  value_angleY: document.querySelector("#value-angleY"),
+  value_angleZ: document.querySelector("#value-angleZ"),
+  value_scaleX: document.querySelector("#value-scaleX"),
+  value_scaleY: document.querySelector("#value-scaleY"),
+  value_scaleZ: document.querySelector("#value-scaleZ"),
+}
+
+export var slider = {
+  slider_transX: document.querySelector("#slider-x"),
+  slider_transY: document.querySelector("#slider-y"),
+  slider_transZ: document.querySelector("#slider-z"),
+  slider_angleX: document.querySelector("#slider-angleX"),
+  slider_angleY: document.querySelector("#slider-angleY"),
+  slider_angleZ: document.querySelector("#slider-angleZ"),
+  slider_scaleX: document.querySelector("#slider-scaleX"),
+  slider_scaleY: document.querySelector("#slider-scaleY"),
+  slider_scaleZ: document.querySelector("#slider-scaleZ"),
+}
+
+export var button = {
+  button_reset: document.querySelector("#reset"),
+}
